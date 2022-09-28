da8cb632d1e8a1217c0fd31b55bdda3a5980577d
Edwin Agana <edwinagana@gmail.com>
2022-09-28 10:26:31 -0700

    error fixed

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    def s_r_elm(elm):
-        return (elm if elm != search else replace)
-    return list(map(s_r_elm, my_list)
+    if my_list:
+        new_list = []
+        for i in my_list:
+            if i == search:
+                i = replace
+                new_list.append(i)
+            else:
+                new_list.append(i)
+        return new_list
+    else:
+        return my_list
